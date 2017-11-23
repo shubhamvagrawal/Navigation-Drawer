905e57e1660b8c67989ef953d6793d85c83d8578
Shubham Agrawal <shubhamvagrawal@gmail.com>
2017-11-23 18:07:23 +0530

    N

---
+++ src/main/java/shubh/navigationdrawer/Fragment_2.java
@@ -0,0 +1,34 @@
+package shubh.navigationdrawer;
+
+/**
+ * Created by $hubh on 11/23/2017.
+ */
+
+import android.os.Bundle;
+import android.view.View;
+import android.support.v4.app.Fragment;
+import android.view.ViewGroup;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+
+public class Fragment_2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.fragment_layout_2, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Gallery ");
+    }
+
+}
